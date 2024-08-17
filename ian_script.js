97b7b7668f56d9b4966fd8f73b9be4e4a6b8bc07
Ian Deans <ian@ideans.dev>
2024-08-16 22:40:45 -0700

    Update ian_script.js
    add on/off light in top right corner. scraper on scrapes when 'on'.

--- ian_script.js
+++ ian_script.js
@@ -12,60 +12,98 @@
 (function () {
   "use strict";
 
-  const post_data = document.getElementsByClassName("contentlook");
-  const posts = Array.from(post_data);
+  const scraperOn = localStorage.getItem("scraperOn");
 
-  const savedPostData = getSavedPostData();
+  const onLight = document.createElement('div');
+  onLight.style.position = 'fixed';
+  onLight.style.top = '0px';
+  onLight.style.right = '0px';
+  onLight.style.width = '50px';
+  onLight.style.height = '50px';
+  onLight.style.backgroundColor = scraperOn === 'on' ? 'green' : 'red';
 
-  posts.forEach((post) => {
-    const post_header = post.previousElementSibling;
+  document.body.appendChild( onLight );
 
-    const postData = {
-      authorName: "",
-      timestamp: "",
-      link: "",
-      contentHTML: "",
-    };
+  if (scraperOn === "on") {
+    scrapePosts();
+  }
 
-    if (post.querySelector("form")) {
-      return;
-    }
+  function scrapePosts() {
+    const postElements = document.getElementsByClassName("contentlook");
+    const posts = Array.from(postElements);
 
-    postData.timestamp = post_header.querySelector("span").innerHTML;
-    postData.link = post_header.querySelector("a").href;
+    const savedPostData = getSavedPostData();
 
-    postData.authorName = post.querySelector("div span a").text;
-    postData.contentHTML = post.querySelector("div:last-of-type").outerHTML;
-    savedPostData.push(postData);
-  });
+    posts.forEach((post) => {
+      const post_header = post.previousElementSibling;
 
-  console.log(savedPostData);
+      const postData = {
+        authorName: "",
+        timestamp: "",
+        link: "",
+        contentHTML: "",
+      };
 
-  storeSavedPostData( savedPostData )
+      if (post.querySelector("form")) {
+        return;
+      }
 
+      postData.timestamp = post_header.querySelector("span").innerHTML;
+      postData.link = post_header.querySelector("a").href;
+
+      postData.authorName = post.querySelector("div span a").text;
+      postData.contentHTML = post.querySelector("div:last-of-type").outerHTML;
+      savedPostData.push(postData);
+    });
+
+    console.log(savedPostData);
+
+    storeSavedPostData(savedPostData);
+  }
 
   function getSavedPostData() {
-    let savedPostData = localStorage.getItem('savedPostData');
-    if ( !savedPostData ) {
+    let savedPostData = localStorage.getItem("savedPostData");
+    if (!savedPostData) {
       return [];
     }
 
     return JSON.parse(savedPostData);
   }
 
-  function storeSavedPostData( savedPostData ) {
-    const stringedData = JSON.stringify( savedPostData );
-    localStorage.setItem( 'savedPostData', stringedData );
+  function storeSavedPostData(savedPostData) {
+    const stringedData = JSON.stringify(savedPostData);
+    localStorage.setItem("savedPostData", stringedData);
   }
 
   function clearSavedPostData() {
-    localStorage.removeItem('savedPostData');
+    localStorage.removeItem("savedPostData");
   }
 
-  document.addEventListener('keydown', event => {
-    console.log(event);
-      if (event.key === '3' && event.altKey) {
-          clearSavedPostData();
-      }
-  })
+  function toggleScript() {
+    let current = localStorage.getItem("scraperOn");
+
+    console.log('getting "current" value');
+    console.log(current);
+
+    if (current === "off") {
+      localStorage.setItem("scraperOn", "on");
+      onLight.style.backgroundColor = 'green';
+    } else {
+      localStorage.setItem("scraperOn", "off");
+      onLight.style.backgroundColor = 'red';
+    }
+  }
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "4" && event.altKey) {
+      console.log("toggle");
+      toggleScript();
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "3" && event.altKey) {
+      clearSavedPostData();
+    }
+  });
 })();
